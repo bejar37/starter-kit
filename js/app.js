04282d0ce765d80cd53a1be0977cffa3633f4fcc
Miguel Bejar <mbejar@Miguels-MacBook-Air.local>
2012-08-23 23:19:37 -0400

    initial commit

---
+++ js/app.js
@@ -0,0 +1,24 @@
+var Welcome = Ember.Application.create({
+  ready: function() {
+    alert('you did it!');
+  }
+});
+
+
+Welcome.Book = Ember.Object.extend({
+  title: '',
+  author: '',
+  genre: ''
+});
+
+Welcome.booksController = Ember.ArrayController.create({
+  content: [],
+  loadBooks: function() {
+    var self = this;
+    $.getJSON('data/books.json', function(data) {
+      data.forEach(function(item){
+        self.pushObject(Welcome.Book.create(item));
+      });
+    });
+  }
+});
